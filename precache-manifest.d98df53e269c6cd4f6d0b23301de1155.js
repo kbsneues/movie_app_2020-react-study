d15105fb92d785a84719537d4505061351c50b0c
kbsneues <kbsneues@naver.com>
2020-07-17 19:46:39 +0900

    Updates

---
+++ precache-manifest.d98df53e269c6cd4f6d0b23301de1155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1dc2c35412dd4a025869f9fc46807b",
+    "url": "/kbsneues/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9f67d1a0cc084127bbba",
+    "url": "/kbsneues/movie_app_2020/static/css/main.6d01c838.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/kbsneues/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kbsneues/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f67d1a0cc084127bbba",
+    "url": "/kbsneues/movie_app_2020/static/js/main.864823c8.chunk.js"
+  },
+  {
+    "revision": "f65bec393f52208831af",
+    "url": "/kbsneues/movie_app_2020/static/js/runtime-main.48d10424.js"
+  }
+]);
